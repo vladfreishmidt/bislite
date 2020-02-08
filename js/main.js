8e48cb680a489fc6f4c0fc99449a5b56c5c6531e
Vlad <vladfreishmidt.webdevelopment@gmail.com>
2020-02-08 12:50:00 +0200

    Testimonials block complete

--- js/main.js
+++ js/main.js
@@ -6,4 +6,13 @@ $(function () {
         arrows: false
     });
 
+    $('.works__inner').slick({
+        dots: false,
+        arrows: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        prevArrow: '<button class="slick-arrow slick-prev"><img src="images/slider/slide-left.svg" alt=""></button>',
+        nextArrow: '<button class="slick-arrow slick-next"><img src="images/slider/slide-right.svg" alt=""></button>'
+    });
+
 });
